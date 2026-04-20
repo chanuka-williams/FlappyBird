a7720de5a491f7150e7eda428803c4264c32a2eb
chanu <chanuka.williams@outlook.com>
2026-04-20 18:32:36 +0100

    Removed layer logic.

--- engine/include/engine/core/Application.h
+++ engine/include/engine/core/Application.h
@@ -1,10 +1,8 @@
 #pragma once
 
-#include <vector>
-
 #include "engine/core/AppSpec.h"
 #include "engine/core/AssetManager.h"
-#include "engine/core/Layer.h"
+#include "engine/core/LayerStack.h"
 
 namespace engine::core
 {
@@ -21,68 +19,11 @@ public:
     void Run();
 
     AssetManager& GetAssetManager();
-
-    void ProcessLayerActions();
-
-    template <std::derived_from<Layer> LayerToPush, typename... Args>
-    void QueueLayerPush(Args&&... args)
-    {
-        auto factory = [this, ... capturedArgs = std::forward<Args>(args)]() mutable {
-            m_layers.emplace_back(std::make_unique<LayerToPush>(std::forward<decltype(capturedArgs)>(capturedArgs)...));
-        };
-
-        m_queuedLayerActions.emplace_back(std::move(factory));
-    }
-
-    template <std::derived_from<Layer> FromLayer, std::derived_from<Layer> ToLayer, typename... Args>
-    void QueueLayerTransition(Args&&... args)
-    {
-        auto factory = [this, ... capturedArgs = std::forward<Args>(args)]() mutable {
-            auto toLayer = std::make_unique<ToLayer>(std::forward<Args>(capturedArgs)...);
-
-            for (auto& layer : m_layers)
-            {
-                if (!dynamic_cast<FromLayer*>(layer.get()))
-                    continue;
-
-                layer = std::move(toLayer);
-                return;
-            }
-        };
-
-        m_queuedLayerActions.push_back(std::move(factory));
-    }
-
-    template <std::derived_from<Layer> LayerToErase>
-    void QueueLayerRemove()
-    {
-        auto factory = [this]() mutable {
-
-            int indexToErase = -1;
-
-            for (int i = 0; i < m_layers.size(); ++i)
-            {
-                if (!dynamic_cast<LayerToErase*>(m_layers[i].get()))
-                    continue;
-
-                indexToErase = i;
-                break;
-            }
-
-            if (indexToErase >= 0)
-            {
-                m_layers.erase(m_layers.begin() + indexToErase);
-            }
-
-        };
-
-        m_queuedLayerActions.push_back(std::move(factory));
-    }
+    LayerStack& GetLayerStack();
 
 private:
     AssetManager m_assetManager{};
-    std::vector<std::unique_ptr<Layer>> m_layers{};
-    std::vector<std::function<void()>> m_queuedLayerActions;
+    LayerStack m_layerStack{};
 };
 
 } // namespace engine::core
